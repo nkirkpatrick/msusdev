f1f7b6a6b7af52ef65d65ebcf5ca1f619abe1394
Norbert Kirkpatrick <norbert_kirkpatrick@yahoo.com>
2022-03-06 18:59:59 -0800

    Run Hello World Script

--- hello_world.py
+++ hello_world.py
@@ -3,7 +3,8 @@ print('         HELLO APP')
 print('-----------------------------')
 print()
 
-user_text = input('What is your name? ')
+# user_text = input('What is your name? ')
+user_text = 'Norbert '
 greeting = 'Nice to meet you ' + user_text
 
 print(greeting)
